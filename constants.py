40826ed245f0c0cf93a7482544730f23dc78beb5
Pawel Jonczyk <pjonczyk07@gmail.com>
2021-05-22 00:01:24 +0200

    helm msg updated

--- constants.py
+++ constants.py
@@ -37,12 +37,6 @@ DATA ENTERING
   but should be greater than 0 and previously entered value
   Entered day (value) will also have start hour as given before entering 'Day'
 
-SETTING NOT WHOLE HOURS
-If you would like to set (for example):
-> 'Work starts' as 6:30 then enter at the beginning '6.5'
-> 'Work starts' as 6:15 then enter at the beginning '6.25'
-> REMEMBER TO USE ' . ' NOT ' , ' as separator
-
 REQUIRED FILES
 For program to work properly you need to have two files in the same directory
 as '.exe' file:
